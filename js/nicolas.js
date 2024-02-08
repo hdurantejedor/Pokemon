ca6cd6703966a2b26c618fc36ecaf0dd55dce02f
NicolasRicoVilla <125301432+NicolasRicoVilla@users.noreply.github.com>
2024-02-08 15:53:52 +0100

    Update nicolas.js
    botones dentro de la ventana detalles pokemon

--- js/nicolas.js
+++ js/nicolas.js
@@ -1,15 +1,35 @@
-function botonVolver(container, pokemonId, direccion) {
-    var enlaceNavegacion = document.createElement('a');
-    enlaceNavegacion.textContent = direccion;
-    enlaceNavegacion.href = '#';
+function crearBotonVolver() {
+    var boton = document.createElement('button');
+    boton.textContent = 'Volver';
+    boton.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+    document.body.appendChild(boton); // Agregar el botón al cuerpo del documento
+}
 
-    enlaceNavegacion.onclick = function () {
-        var siguientePokemonId = (direccion === 'Siguiente') ? pokemonId + 1 : pokemonId - 1;
-        mostrarDetalles(siguientePokemonId);
-        return false;
-    };
+// Función para crear botones de navegación y agregarlos al contenedor especificado
+function crearBotonesNavegacion(pokemonId) {
+    var botonesContainer = document.createElement('div');
+    botonesContainer.id = 'botones-navegacion';
 
+    // Crear botón "Anterior"
+    var botonAnterior = document.createElement('button');
+    botonAnterior.textContent = 'Anterior';
+    botonAnterior.addEventListener("click", function () {
+        var pokemonAnteriorId = pokemonId - 1;
+        mostrarDetalles(pokemonAnteriorId);
+    });
+    botonesContainer.appendChild(botonAnterior);
 
+    // Crear botón "Siguiente"
+    var botonSiguiente = document.createElement('button');
+    botonSiguiente.textContent = 'Siguiente';
+    botonSiguiente.addEventListener("click", function () {
+        var pokemonSiguienteId = pokemonId + 1;
+        mostrarDetalles(pokemonSiguienteId);
+    });
+    botonesContainer.appendChild(botonSiguiente);
 
-    container.appendChild(enlaceNavegacion);
+    // Agregar los botones al cuerpo del documento
+    document.body.appendChild(botonesContainer);
 }
