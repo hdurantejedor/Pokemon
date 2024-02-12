1b31be2bce0f90d21f49f81c43d65eb9a1df456b
alvarorojopelarda <alvarorp.150@gmail.com>
2024-02-12 17:24:00 +0100

    .

--- js/alvaro.js
+++ js/alvaro.js
@@ -86,42 +86,44 @@ window.onclick = function (event) {
         }
     }
 }
+function cargarPokemons() {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${pokemonesPorPagina}`)// Ajusta el límite según sea necesario
+        .then(response => response.json())
+        .then(data => {
+            pokemonList = data.results; // Asegúrate de que esto se ajuste a tus necesidades
+            mostrarPokemons(); // Muestra los Pokémon recién cargados
+        })
+        .catch(error => manejarError(error.message));
+}
+
 function sort(criterio) {
+    // Asumiendo que pokemonList ya está lleno
     switch (criterio) {
         case 'lowerNumber':
-            pokemonList.sort((a, b) => a.numero - b.numero);
-            console.log("Lista ordenada por número ascendente: ");
-            
+            // Ordenamiento por ID más bajo a más alto
+            pokemonList.sort((a, b) => parseInt(a.url.split('/')[6]) - parseInt(b.url.split('/')[6]));
             break;
         case 'higherNumber':
-            pokemonList.sort((a, b) => b.numero - a.numero);
+            // Ordenamiento por ID más alto a más bajo
+            pokemonList.sort((a, b) => parseInt(b.url.split('/')[6]) - parseInt(a.url.split('/')[6]));
             break;
-        case 'aToZ':
-            pokemonList.sort((a, b) => a.nombre.localeCompare(b.nombre));
-            break;
-        case 'zToA':
-            pokemonList.sort((a, b) => b.nombre.localeCompare(a.nombre));
-            break;
+        // Agregar más casos según sea necesario
     }
-    
-    mostrarPokemonsOrdenados();
+    mostrarPokemonsOrdenados(); // Actualiza la UI con la lista ordenada
 }
 
 function mostrarPokemonsOrdenados() {
     const contenedor = document.getElementById("capa");
-    contenedor.innerHTML = ""; // Limpia el contenedor de Pokémon para preparar la nueva visualización ordenada
+    contenedor.innerHTML = ''; // Limpia el contenedor
 
-    // Asumiendo que `listaPokemon` es un arreglo de objetos Pokémon que ya ha sido ordenado
     pokemonList.forEach(pokemon => {
-        // Crear y añadir un nuevo elemento para cada Pokémon en la lista ordenada
-        var contenedorPokemon = document.createElement("div");
-        contenedorPokemon.className = "pokemon-box";
-        contenedorPokemon.innerHTML = `
-            <p><span style="cursor: pointer; text-decoration: underline;">${nombre} (#${id})</span></p>
-            <img src="${imagen}" alt="${nombre}">
-        `;
-
-        contenedor.appendChild(contenedorPokemon);
+        fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokemonData => {
+                // Suponiendo que procesarPokemon actualiza la UI
+                procesarPokemon(pokemonData);
+            })
+            .catch(error => manejarError(error.message));
     });
 }
 
